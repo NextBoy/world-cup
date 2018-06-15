8b94687c8cb2a501b67e018359a7190bd861e336
zhuozhongzhi <zhongzhi_z@gzleihou.cn>
2018-06-15 18:31:52 +0800

    Initial commit

---
+++ src/components/race-list.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <header>
+      <img src="../images/hd.png" alt="">
+      <h1>精彩赛事</h1>
+    </header>
+    <div class="card-item-wrap">
+      <div class="date">2017-1-12 <span>23:00</span></div>
+      <div class="card-item">
+        <div class="left box">
+          <img src="http://placehold.it/50x35" class="flag">
+          <p>中国</p>
+        </div>
+        <div class="not-start center box">
+          <p>预约提醒</p>
+          <p>VS</p>
+        </div>
+        <div class="right box">
+          <img src="http://placehold.it/50x35" class="flag">
+          <p>中国</p>
+        </div>
+      </div>
+    </div>
+    <div class="card-item-wrap">
+      <div class="date">2017-1-12 <span>23:00</span></div>
+      <div class="card-item">
+        <div class="left box">
+          <img src="http://placehold.it/50x35" class="flag">
+          <p>中国</p>
+        </div>
+        <div class="over center box">5 - 6</div>
+        <div class="right box">
+          <img src="http://placehold.it/50x35" class="flag">
+          <p>中国</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'race-list',
+    props: ['list']
+  }
+</script>
+<style lang="less" scoped>
+  header {
+    text-align: center;
+    img {
+      width: 90vw;
+      height: 130px;
+      border-radius: 5px;
+      margin: 10px auto;
+    }
+    padding-bottom: 15px;
+  }
+  .card-item-wrap {
+    .date {
+      text-align: center;
+      color: deepskyblue;
+      span {
+        color: red;
+        font-weight: bold;
+      }
+    }
+  }
+  .card-item {
+    margin: 0 auto;
+    width: 85vw;
+    height: 70px;
+    padding: 10px 6px;
+    margin-bottom: 10px;
+    background: white;
+    box-shadow: 0 4px 16px 0 rgba(18, 21, 58, 0.14);
+    border-radius: 6px;
+    display: flex;
+    flex-direction: row;
+    .box {
+      height: 70px;
+      flex: 1;
+      text-align: center;
+    }
+    .left {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      p {
+        line-height: 1.5;
+      }
+    }
+    .right {
+      .left;
+    }
+    .center {
+      width: 50%;
+      border-radius: 5px;
+      .left;
+    }
+    .not-start {
+      background: dodgerblue;
+      color: white;
+    }
+    .over {
+      color: white;
+      background: gray;
+      font-size: 25px;
+      font-weight: bold;
+    }
+    .flag {
+      width: 50px;
+      height: 35px;
+    }
+  }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <raceList></raceList>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+import raceList from '@/components/race-list'
+
+export default {
+  components: {
+    raceList
+  },
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+  .background {
+    position: fixed;
+    z-index: -1;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+  }
+</style>
